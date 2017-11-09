b9e4ccea5ae2c9d8d0c134a2fe03492dce201d85
fakhri <arifin.fakhri.h@gmail.com>
2017-11-10 02:02:13 +0700

    Edit
    menambahkan narasi tentang kasus yang dikerjakan

--- watermelon.py
+++ watermelon.py
@@ -1,34 +1,13 @@
-##x= input("");
-# x= int(x);
-# if (x%2!=0):
-#     print "NO";
-# elif(x<=2):
-#     print "NO";
-# else:
-#     print "YES"
-##if x%2==0 and x>2:
-##    print "YES";
-##else:
-##    print "NO";
+#Untuk mencar tahu apakah  semangka bisa dibagi ke lebih dari 2 orang dengan bagian yang sama
+x= input("");
+x= int(x);
+if (x%2!=0):
+    print "NO";
+elif(x<=2):
+    print "NO";
+else:
+    print "YES"
 
-def solution(a,x):
-    n=len(a)
-    if n==0:
-        return -1
-    l=0
-    r=n-1;
-    while l<r :
-        m =(l+r)//2
-        if a[m] != x:
-            r=m-1
-        else:
-            l=m
-
-    if a[l]==x:
-        return l
-    return -1
-a=[1,2,5,9,9]
-print solution(a,1)
 
 
 
